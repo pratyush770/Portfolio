ef350f8e1056e75d9f31a8f80081af5f5170d823
pratyush770 <pratyushmajumdar27@gmail.com>
2022-12-17 22:52:04 +0530

    first committ

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-    ]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
